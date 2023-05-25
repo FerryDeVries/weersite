d283e5e6daa3a69861fdd344fc83e00c20419247
FerryDeVries <71122367+FerryDeVries@users.noreply.github.com>
2023-05-25 13:00:45 +0200

    added footer

---
+++ public/footer.php
@@ -0,0 +1,34 @@
+<?php require __DIR__ . '/../backend/config.php' ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Movie Website</title>
+</head>
+<body class="flex flex-col min-h-screen">
+    <header class="bg-gray-900">
+        <!-- Header navigation bar code goes here -->
+    </header>
+
+    <!-- Rest of your website content goes here -->
+
+    <footer class="bg-gray-900 mt-auto">
+        <div class="container mx-auto py-6">
+            <div class="flex flex-col md:flex-row md:justify-between">
+                <div class="mb-4 md:mb-0">
+                    <span class="text-white">Movie Website &copy; <?php echo date('Y'); ?></span>
+                </div>
+                <div>
+                    <ul class="flex space-x-4">
+                        <li><a href="#" class="text-white hover:text-gray-300">Privacy Policy</a></li>
+                        <li><a href="#" class="text-white hover:text-gray-300">Terms of Service</a></li>
+                        <li><a href="#" class="text-white hover:text-gray-300">Contact Us</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
